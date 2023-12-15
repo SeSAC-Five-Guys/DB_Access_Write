baa11360de7e6bf89378233526412e16de2d5aaf
hb9397 <hb9397@naver.com>
2023-12-16 00:29:12 +0900

    🎉init: DB_Access_Write 서버 애플리케이션 생성

---
+++ src/main/java/com/sesac/db_access_write/DbAccessWriteApplication.java
@@ -0,0 +1,13 @@
+package com.sesac.db_access_write;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbAccessWriteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbAccessWriteApplication.class, args);
+	}
+
+}
