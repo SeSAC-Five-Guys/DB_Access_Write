f89c96525a55fd66f2c53a181d288c736da7b786
hb9397 <hb9397@naver.com>
2024-01-08 02:28:44 +0900

    ✨ feat: 회원 정보 수정에서 본인의 원래 정보는 변경 가능한 중복 확인 API 추가
    - 1.0.0

--- src/main/java/com/sesac/member_write_server/member/controller/MemberController.java
+++ src/main/java/com/sesac/member_write_server/member/controller/MemberController.java
@@ -97,10 +97,37 @@ public class MemberController {
 		return new ResponseEntity<>(response, status);
 	}
 
+	/* 회원 정보 수정 중복 확인*/
+	@GetMapping("/nickname/{nick1}/{nick2}")
+	public ResponseEntity<ResDto> isDuplicatedNicknameInModify(
+		@Valid
+		@Pattern(regexp = "^(?=.[가-힣a-zA-Z])[-가-힣a-zA-Z0-9!@#$%&]{5,12}$|(?=.[가-힣])[-가-힣0-9!@#$%&]{5,12}$|(?=.*[a-zA-Z])[-a-zA-Z0-9!@#$%&]{5,12}$", message = "잘못된 닉네임 형식")
+		@NotBlank(message = "닉네임 공백 포함 불가")
+		@PathVariable("nick1") String currentNick,
+		@PathVariable("nick2") String changeNick
+	){
+		ResDto response = memberService.isDuplicatedNicknameInModify(currentNick, changeNick);
+		HttpStatus status = makeResult.changeStatus(response);
+		return new ResponseEntity<>(response, status);
+	}
+	@GetMapping("/phone_number/{phoneNum1}/{phoneNum2}")
+	public ResponseEntity<ResDto> isDuplicatedPhoneNumInModify(
+		@Valid
+		@Pattern(regexp = "^01[0-9]{9}$", message = "잘못된 핸드폰 번호 형식")
+		@NotBlank(message = "핸드폰 번호 공백 포함 불가")
+		@PathVariable("phoneNum1") String currentPhoneNum,
+		@PathVariable("phoneNum2") String changePhoneNum
+	){
+		ResDto response = memberService.isDuplicatedPhoneNumInModify(currentPhoneNum, changePhoneNum);
+		HttpStatus status = makeResult.changeStatus(response);
+		return new ResponseEntity<>(response, status);
+	}
+
 
 	/* 회원 정보 수정 */
 	@PutMapping("/member/{email}")
 	public ResponseEntity<ResDto> memberModifytAndSendMQ(
+		@Valid
 		@Pattern(regexp = "^[a-zA-Z0-9]+@[a-zA-Z0-9]+\\.[A-Za-z]+$", message = "잘못된 이메일 형식")
 		@Size(max = 100, message = "이메일 길이 초과")
 		@NotBlank(message = "이메일 공백 포함 불가")
@@ -115,6 +142,7 @@ public class MemberController {
 	/* 회원 정보 삭제	*/
 	@DeleteMapping("/member/{email}")
 	public ResponseEntity<ResDto> memberDeleteAnsSendMQ(
+		@Valid
 		@Pattern(regexp = "^[a-zA-Z0-9]+@[a-zA-Z0-9]+\\.[A-Za-z]+$", message = "잘못된 이메일 형식")
 		@Size(max = 100, message = "이메일 길이 초과")
 		@NotBlank(message = "이메일 공백 포함 불가")
